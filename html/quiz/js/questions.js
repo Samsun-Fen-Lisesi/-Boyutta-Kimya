8eb400318938f3bd20ec0dfc97baac56e82430a8
root <taliptaha06@gmail.com>
2022-03-05 16:45:20 +0000

    Initial Commit

---
+++ html/quiz/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Aşağıdakilerden hangisi zayıf etkileşimlerden değildir?",
+    answer: "Metalik bağ",
+    options: [
+      "Van der Waals bağları",
+      "Dipol-dipol bağları",
+      "Metalik bağ",
+      "İyon-dipol bağı"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Aşağıdaki seçeneklerin hangisinde kimyasal türün örneği yanlış verilmiştir?",
+    answer: "Bileşik - H2",
+    options: [
+      "Atom - Ca",
+      "Molekül - CO2",
+      "Molekül - CO",
+      "Bileşik - H2"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Aşağıdaki bileşiklerden hangisinde atomlar arası apolar kovalent bağ, moleküller arası sadece London kuvvetleri bulunur? ( 1H, 6C, 7N, 8O, 11Na, 17Cl, 9F)",
+    answer: "O2",
+    options: [
+      "HF",
+      "NH3",
+      "O2",
+      "CH3Cl"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Aşağıdaki bileşiklerden hangisi iyonik yapılıdır? (1H, 3Li, 5B, 6C, 8O, 9F, 15P, 16S, 17Cl, 35Br)",
+    answer: "LiBr",
+    options: [
+      "CS2",
+      "BF3",
+      "LiBr",
+      "PCl5"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Elektron dağılımı X: 2) 8) 2) olan element ile elektron dağılımı Y: 2) 7) olan element arasında oluşacak bileşik ile ilgili aşağıdaki bilgilerden hangisi yanlıştır?",
+    answer: "Bileşiğin formülü X2Y dir.",
+    options: [
+      "İyonik bağ yapar",
+      "Molekül yapısı yoktur.",
+      "Sulu çözeltisi elektriği iletir.",
+      "Bileşiğin formülü X2Y dir."
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
